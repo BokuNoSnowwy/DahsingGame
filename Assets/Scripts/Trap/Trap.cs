497eba6df3292f26967a0ddfd6f7a333dd181095
Ryfast <60229010+Ryfast@users.noreply.github.com>
2023-05-10 14:36:19 +0200

    Trap done

---
+++ Assets/Scripts/Trap/Trap.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour, IInteractable
+{
+
+    public Vector2 iniPos { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public void DetectPlayer()
+    {
+        throw new System.NotImplementedException();
+        //Destroy(other.gameObject);
+
+    }
+
+    public void ListenEventGameManager()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResetInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
