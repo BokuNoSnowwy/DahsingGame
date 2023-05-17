fdcabcaa25fd0b3342b62ccb1aface823eda82ef
Mornet Valentin <valentin.mornet@gamabilis.com>
2023-05-17 11:14:46 +0200

    finishing line Script creation

---
+++ Assets/Scripts/Interactables/FinishingLineInteractable.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishingLineInteractable : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector2 iniPos { get; set; }
+    
+    public void DetectPlayer()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResetInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ListenEventGameManager()
+    {
+        throw new System.NotImplementedException();
+    }
+}
