b9601f7982966a31d52fa38a83965444bba5c96c
Joldd <julien.coutouly@gmail.com>
2023-05-31 11:08:32 +0200

    Detection Sprite player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -31,6 +31,7 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(spriteRenderer.isVisible);
         if (!spriteRenderer.isVisible && isAlive)
         {
             Die();
